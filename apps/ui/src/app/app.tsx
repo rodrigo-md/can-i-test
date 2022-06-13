5acca7d2eba6dfdbff121ad3f6f62f1321c6b746
Rodrigo Martinez Diaz <martinez.d.rodrigo@gmail.com>
2022-06-13 01:04:11 -0400

    chore: setup with nx

---
+++ apps/ui/src/app/app.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import NxWelcome from './nx-welcome';
+import { useState, useEffect } from 'react'
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  const [response, setResponse] = useState({message: 'loading...'});
+
+  useEffect(() => {
+    fetch('/api')
+      .then(response => response.json())
+      .then(setResponse);
+  }, []);
+
+  return (
+    <StyledApp>
+      {/* <NxWelcome title={response.message} /> */}
+      <div>{response.message}</div>
+    </StyledApp>
+  );
+}
+
+export default App;
