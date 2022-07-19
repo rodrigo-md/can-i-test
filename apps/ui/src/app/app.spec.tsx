d77eb7505b3061c4bc2ff4e7b6b03ed61e38ed6b
Rodrigo Martinez Diaz <martinez.d.rodrigo@gmail.com>
2022-07-19 03:59:21 -0400

    test(ui): mock fetch using jest-fetch-mock

--- apps/ui/src/app/app.spec.tsx
+++ apps/ui/src/app/app.spec.tsx
@@ -1,11 +1,19 @@
-import { render } from '@testing-library/react';
+import { render, waitFor, screen } from '@testing-library/react';
 
 import App from './app';
+import jestFetchMock from 'jest-fetch-mock'
+
+jestFetchMock.enableMocks();
 
 describe('App', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<App />);
 
-    expect(baseElement).toBeTruthy();
+  it('should render successfully', async () => {
+    fetchMock.mockResponse(JSON.stringify({ message: 'Can I help App' }));
+
+    render(<App />);
+
+    await waitFor(() => {
+      expect(screen.getByText(/Can I help App/i)).toBeTruthy();
+    });
   });
 });
