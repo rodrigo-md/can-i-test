ce60678bb6494ddb4ca09b82b9d6ff013ec4e442
Rodrigo Martinez Diaz <martinez.d.rodrigo@gmail.com>
2022-07-29 02:19:04 -0400

    refactor(github client): separate client methods into multiple files one method equals one file

---
+++ apps/api/src/adapters/clients/github/get-user.ts
@@ -0,0 +1,37 @@
+import { Forbidden, RequiresAuthentication } from './errors';
+import type { HttpClient } from './interfaces/dependencies';
+import type { GithubUser } from './interfaces/user';
+
+export function createGetUser(httpClient: HttpClient) {
+  return async (accessToken: string) => {
+    try {
+      const response = await httpClient.get<GithubUser>(
+        'https://api.github.com/user',
+        {
+          headers: {
+            authorization: `bearer ${accessToken}`,
+            accept: 'application/vnd.github+json',
+          },
+        },
+      );
+
+      return {
+        username: response.data.login,
+        avatarUrl: response.data.avatar_url,
+        homepage: response.data.html_url,
+      };
+    } catch (e) {
+      if (e.response && e.response.status) {
+        switch (e.response.status) {
+          case 401: {
+            throw new RequiresAuthentication();
+          }
+          case 403: {
+            throw new Forbidden();
+          }
+        }
+      }
+      throw e;
+    }
+  };
+}
